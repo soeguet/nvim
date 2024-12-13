9a18f343f9407a09d3683d817a7f2e4366b7f62e
soeguet <osman_s@live.de>
2024-12-13 22:34:49 +0100

    update

--- lua/plugins/painless-java.lua
+++ lua/plugins/painless-java.lua
@@ -8,6 +8,18 @@ return {
         servers = {
           jdtls = {
             -- Your custom jdtls settings goes here
+            handlers = {
+              -- By assigning an empty function, you can remove the notifications
+              -- printed to the cmd
+              ["$/progress"] = function(_, result, ctx) end,
+            },
+            settings = {
+              java = {
+                format = {
+                  enabled = false,
+                },
+              },
+            },
           },
         },
         setup = {
